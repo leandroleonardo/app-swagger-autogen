4b1b640a2f7893c79e4324b12a487af07c48e076
Leandro L Assarice <57691941+leandroleonardo@users.noreply.github.com>
2023-10-02 00:04:08 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,9 +5,28 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
 const port = process.env.PORT
 const app = express()
+const users = require('./users.json')
 
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
-require('./endpoints')(app)
+app.get('/users/:id', async (req, res) => {
+
+    users.forEach(user => {
+        if ( user.id === req.params.id ) return res.json(user)
+    });
+
+    res.status(404)
+})
+
+app.get('/users/', (req, res) => {
+
+    if (users) {
+        return res.status(200).json(users)
+    } else {
+        res.status(404)
+    }
+})
+
+//require('./endpoints')(app)
 
 app.listen(port, () => console.log(`Listening on port ${port}`))
