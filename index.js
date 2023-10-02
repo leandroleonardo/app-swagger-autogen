f77db1d501f2a9b90b9535fb9cebf40a0d6a3a5f
Leandro L Assarice <57691941+leandroleonardo@users.noreply.github.com>
2023-10-01 23:55:27 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-const app = require('express')()
-const http = require('http')
+require('dotenv').config()
+
+const express = require('express')
 const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
-require('dotenv').config()
 const port = process.env.PORT
-
-http.createServer(app).listen(port)
-console.log("Listening at:// port:%s (HTTP)", port)
+const app = express()
 
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 require('./endpoints')(app)
+
+app.listen(port, () => console.log(`Listening on port ${port}`))
