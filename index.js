1cf998b82c92a38b6656a0044f18c5e394bd4475
Leandro L Assarice <57691941+leandroleonardo@users.noreply.github.com>
2023-10-02 00:36:31 -0300

    Update index.js - cors

--- index.js
+++ index.js
@@ -5,31 +5,15 @@ const swaggerUi = require('swagger-ui-express')
 const swaggerFile = require('./swagger_output.json')
 const port = process.env.PORT
 const app = express()
+const cors = require('cors')
 const users = require('./users.json')
 
+app.use(cors())
 app.use('/doc', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 app.get('/', (req, res) => {
     res.json('Hello World')
 })
-/*
-app.get('/users/:id', async (req, res) => {
-
-    users.forEach(user => {
-        if ( user.id === req.params.id ) return res.json(user)
-    });
-
-    res.status(404)
-})
-
-app.get('/users/', (req, res) => {
-
-    if (users) {
-        return res.status(200).json(users)
-    } else {
-        res.status(404)
-    }
-})*/
 
 require('./endpoints')(app)
 
